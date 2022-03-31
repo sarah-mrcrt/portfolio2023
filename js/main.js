9181d92fd1e877fc2b0e499b2d8931766e780e04
unknown <mauriaucourt.sarah@gmail.com>
2022-03-31 12:03:52 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,65 @@
+jQuery(document).ready(function ($) {
+
+    // Il est nécessaire de recharger la page, lorsqu'on teste le responsive et qu'on passe au-dessus ou en dessous d'une largeur de 1024px. Sinon le menu burger ne fonctionne pas !
+    window.onresize = function (event) {
+        // Actualisation automatique de la page lorsque la taille de la fenêtre change 
+        document.location.reload(true);
+    }
+
+    if (window.matchMedia("(max-width: 1024px)").matches) {
+
+        // Créer du menu burger, en ajoutant une balise <menu></menu> autour de la balise <nav></nav>
+        $("#header nav").wrapAll("<menu></menu>");
+        //Déplace le place btn#close comme premier enfant de <menu></menu>> 
+        $("#close").prependTo("#header menu");
+        //Déplace le place .social-media comme dernier enfant de <menu></menu>> 
+        $("#header .social-media").insertAfter("#header menu nav");
+
+        // état à l'ouverture du menu
+        $("#open").click(function () {
+            $("#header menu").addClass("open")
+        });
+
+        // état à la fermeture du menu
+        $("#close, #header ul li a").click(function () {
+            $("#header menu").removeClass("open");
+        });
+    }
+});
+
+// gallery item filter
+
+const filterButtons = document.querySelector("#filter-btns").children;
+const items = document.querySelector(".portfolio-gallery").children;
+
+for (let i = 0; i < filterButtons.length; i++) {
+    filterButtons[i].addEventListener("click", function () {
+        for (let j = 0; j < filterButtons.length; j++) {
+            filterButtons[j].classList.remove("active")
+        }
+        this.classList.add("active");
+        const target = this.getAttribute("data-target")
+
+        for (let k = 0; k < items.length; k++) {
+            // Items DISABLED
+            // items[k].style.display = "none";
+            items[k].style.opacity = ".5";
+            items[k].style.pointerEvents = "none";
+            items[k].style.background = "lightgrey";
+
+            if (target == items[k].getAttribute("data-id")) {
+                // Items ACTIF
+                // items[k].style.display = "block";
+                items[k].style.opacity = "1";
+                items[k].style.pointerEvents = "auto";
+                items[k].style.background = "black";
+            }
+            if (target == "all") {
+                // items[k].style.display = "block";
+                items[k].style.opacity = "1";
+                items[k].style.background = "white";
+                items[k].style.pointerEvents = "auto";
+            }
+        }
+    })
+}
