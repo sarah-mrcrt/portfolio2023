6be357f129bf370e9b84ebeee04df4ac9064ac4b
unknown <mauriaucourt.sarah@gmail.com>
2022-04-10 23:59:15 +0200

    debut responsive projets

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 jQuery(document).ready(function ($) {
+    //-------------------------
+    // HEADER
+    //-------------------------
 
-    // Il est nécessaire de recharger la page, lorsqu'on teste le responsive et qu'on passe au-dessus ou en dessous d'une largeur de 1024px. Sinon le menu burger ne fonctionne pas !
     window.onresize = function (event) {
+        // Il est nécessaire de recharger la page, lorsqu'on teste le responsive et qu'on passe au-dessus ou en dessous d'une largeur de 1024px. Sinon le menu burger ne fonctionne pas !
         // Actualisation automatique de la page lorsque la taille de la fenêtre change 
         document.location.reload(true);
     }
@@ -25,41 +28,44 @@ jQuery(document).ready(function ($) {
             $("#header menu").removeClass("open");
         });
     }
+
+    //-------------------------
+    // PROJETS Filtres
+    //-------------------------
+    if ($('.filter-buttons').length) {
+        const filterButtons = document.querySelector("#filter-btns").children;
+        const items = document.querySelector(".portfolio-gallery").children;
+
+        for (let i = 0; i < filterButtons.length; i++) {
+            filterButtons[i].addEventListener("click", function () {
+                for (let j = 0; j < filterButtons.length; j++) {
+                    filterButtons[j].classList.remove("active")
+                }
+                this.classList.add("active");
+                const target = this.getAttribute("data-target")
+
+                for (let k = 0; k < items.length; k++) {
+                    // Items DISABLED
+                    items[k].style.opacity = ".5";
+                    items[k].style.pointerEvents = "none";
+                    items[k].style.background = "lightgrey";
+                    items[k].style.borderTop = "2px solid black";;
+
+                    if (target == items[k].getAttribute("data-id")) {
+                        // Items ACTIF
+                        items[k].style.opacity = "1";
+                        items[k].style.pointerEvents = "auto";
+                        items[k].style.background = "black";
+                        items[k].style.borderTop = "2px solid white";
+                    }
+                    if (target == "all") {
+                        items[k].style.opacity = "1";
+                        items[k].style.pointerEvents = "auto";
+                        items[k].style.background = "white";
+                        items[k].style.borderTop = "2px solid black";
+                    }
+                }
+            })
+        }
+    }
 });
-
-// gallery item filter
-
-const filterButtons = document.querySelector("#filter-btns").children;
-const items = document.querySelector(".portfolio-gallery").children;
-
-for (let i = 0; i < filterButtons.length; i++) {
-    filterButtons[i].addEventListener("click", function () {
-        for (let j = 0; j < filterButtons.length; j++) {
-            filterButtons[j].classList.remove("active")
-        }
-        this.classList.add("active");
-        const target = this.getAttribute("data-target")
-
-        for (let k = 0; k < items.length; k++) {
-            // Items DISABLED
-            items[k].style.opacity = ".5";
-            items[k].style.pointerEvents = "none";
-            items[k].style.background = "lightgrey";
-            items[k].style.borderTop = "2px solid black";;
-
-            if (target == items[k].getAttribute("data-id")) {
-                // Items ACTIF
-                items[k].style.opacity = "1";
-                items[k].style.pointerEvents = "auto";
-                items[k].style.background = "black";
-                items[k].style.borderTop = "2px solid white";
-            }
-            if (target == "all") {
-                items[k].style.opacity = "1";
-                items[k].style.pointerEvents = "auto";
-                items[k].style.background = "white";
-                items[k].style.borderTop = "2px solid black";
-            }
-        }
-    })
-}
