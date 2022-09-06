f9905e7a22f45bc6b93a6482d945269576825f98
unknown <mauriaucourt.sarah@gmail.com>
2022-09-06 17:03:38 +0200

    update

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -213,10 +213,9 @@
             </div>
         </footer>
 
-        <!-- Scripts -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- ------- Scripts ------- -->
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-        <script src="{{ asset('js/script.js')}}"></script>
+        <script src="{{ asset('js/app.js')}}"></script>
     </body>
 </html>
